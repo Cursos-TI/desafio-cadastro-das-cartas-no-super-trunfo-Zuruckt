06827013ca1b0ce94e2cf3f718feddb4e4bac4f5
Jeferson Oliveira <zuruckyt@gmail.com>
2025-04-03 23:50:37 -0300

    fix: add missing data and fix memory leak

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 struct Card {
+    char state;
     char code[3];
+    char name[21];
     int population;
     float area;
     float gdp;
@@ -16,8 +18,14 @@ int main() {
 
     printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
 
+    printf("Digite o Estado da Primeira Carta: ");
+    scanf(" %c", &cardOne.state);
+
     printf("Digite o Código da Primeira Carta: ");
-    scanf(" %3s", cardOne.code);
+    scanf(" %2s", cardOne.code);
+
+    printf("Digite o Nome da Primeira Carta: ");
+    scanf(" %20s", cardOne.name);
 
     printf("Digite a População da Primeira Carta: ");
     scanf(" %d", &cardOne.population);
@@ -33,8 +41,14 @@ int main() {
 
     printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
 
+    printf("Digite o Estado da Segunda Carta: ");
+    scanf(" %c", &cardTwo.state);
+
     printf("Digite o Código da Segunda Carta: ");
-    scanf(" %3s", cardTwo.code);
+    scanf(" %2s", cardTwo.code);
+
+    printf("Digite o Nome da Segunda Carta: ");
+    scanf(" %20s", cardTwo.name);
 
     printf("Digite a População da Segunda Carta: ");
     scanf(" %d", &cardTwo.population);
@@ -50,14 +64,16 @@ int main() {
 
     printf("=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=\n");
 
-    printf("Carta %s \n", cardOne.code);
+    printf("Carta %c%s \n", cardOne.state, cardOne.code);
+    printf("Nome: %s\n", cardOne.name);
     printf("População: %d\n", cardOne.population);
     printf("Área: %.2f\n", cardOne.area);
     printf("PIB: %.2f\n", cardOne.gdp);
     printf("Pontos Turísticos: %d\n", cardOne.poi);
     printf("\n");
 
-    printf("Carta %s \n", cardTwo.code);
+    printf("Carta %c%s \n", cardTwo.state, cardTwo.code);
+    printf("Nome: %s\n", cardTwo.name);
     printf("População: %d\n", cardTwo.population);
     printf("Área: %.2f\n", cardTwo.area);
     printf("PIB: %.2f\n", cardTwo.gdp);
